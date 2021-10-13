ad86320c34334c699c24fab0b96da1433a0f8ac1
alejandrogm02 <78848296+alejandrogm02@users.noreply.github.com>
2021-10-13 08:23:37 +0200

    Update download.php

--- headers/download.php
+++ headers/download.php
@@ -4,5 +4,6 @@ header('Content-Length: 1000000');
 header('Content-Disposition: attachment; filename="download.zip');
 for($i = 0; $i < 1000; $i++){
     echo str_repeat(".",1000);
-}
 usleep(50000);
+}
+?>
